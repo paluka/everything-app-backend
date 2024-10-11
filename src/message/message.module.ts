e7586b6e839ed0f692736ec417dda91fbff37c11
Erik Paluka <erik.paluka@gmail.com>
2024-10-11 17:41:57 +0700

    chore(message module): add missing RabbitMQ service import

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
+import { RabbitMQModule } from 'src/rabbitmq/rabbitmq.module';
 
 @Module({
+  imports: [RabbitMQModule],
   controllers: [MessageController],
   providers: [MessageService],
 })
